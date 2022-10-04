256c70742d6cf59a3078dc0628b23dd00d5bd7fa
jamesonknutson <38028563+jamesonknutson@users.noreply.github.com>
2022-10-03 21:06:22 -0700

    Feature request: User-defined list of taboutable characters/strings in extension settings Fixes #35

--- src/CharacterSet.ts
+++ src/CharacterSet.ts
@@ -1,10 +1,39 @@
-export class CharacterSet
-{
-	open:string;
-	close:string;
-	constructor(o:string, c:string)
-	{
-		this.open = o;
-		this.close = c;		
-	}	
+import * as vscode from 'vscode'
+
+export class CharacterSet {
+  public open: string
+  public close: string
+  constructor (o: ICharacterSet)
+  constructor (o: string, c: string)
+  constructor (o: string | ICharacterSet, c?: string) {
+    if (typeof o === 'string') {
+      this.open = o
+      this.close = c
+    } else {
+      this.open = o.open
+      this.close = o.close
+    }
+  }
+
+  public static loadCharacterSets (): Array<CharacterSet> {
+    return vscode.workspace.getConfiguration(`tabout`).get<ICharacterSet[]>(`charactersToTabOutFrom`, [
+      { open: '[', close: ']' },
+      { open: '{', close: '}' },
+      { open: '(', close: ')' },
+      { open: '\'', close: '\'' },
+      { open: '"', close: '"' },
+      { open: ':', close: ':' },
+      { open: '=', close: '=' },
+      { open: '>', close: '>' },
+      { open: '<', close: '<' },
+      { open: '.', close: '.' },
+      { open: '`', close: '`' },
+      { open: ';', close: ';' },
+    ]).map(o => new CharacterSet(o))
+  }
+}
+
+export interface ICharacterSet {
+  open: string
+  close: string
 }
